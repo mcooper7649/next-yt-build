57000258f01469c25349e5b39a6b8a34bcf0a043
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-09-09 12:56:17 -0400

    complete setup sanity, create first post, add schema and pull in post and style

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header>
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
       <div className="flex items-center space-x-5">
         <Link href="/">
           <img
@@ -20,7 +20,14 @@ const Header = () => {
           </h3>
         </div>
       </div>
-      <div></div>
+      <div>
+        <div className="flex items-center space-x-5 text-green-600">
+          <h3>Sign In</h3>
+          <h3 className="border px-4 py-1 rounded-full border-green-600">
+            Get Started
+          </h3>
+        </div>
+      </div>
     </header>
   );
 };
